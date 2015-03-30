da904d94d109b587305d93508fc86a52edf3600c
Cory Althoff <coryedwardalthoff@gmail.com>
2015-03-29 23:23:08 -0700

    checkin

--- example/example.py
+++ example/example.py
@@ -7,8 +7,14 @@ if __name__ == '__main__':
     client = OrientDB(app=app, password='B0FC9CF1CBEAD07351C4C30197C43BE2D611E94AFAFA7EF4B4AAD3262F7907DB')
     if not client.db_exists('test_db'):
         client.db_create('test_db')
+
+
     db_list = client.db_list()
     print db_list
-    client.db_open('cory')
-    client.command("create class Animal extends V")
-    client.command("insert into Animal set name = 'rat', specie = 'rodent'")
+    # print client.app.config.get('ORIENTDB_CURRENT_DATABASE')
+    client['ORIENTDB_CURRENT_DATABASE'] = 'cory3'
+    # print client.app.config.get('ORIENTDB_CURRENT_DATABASE')
+    print client.db_size()
+    # client.db_open('test_db')
+    # client.command("create class Animal extends V")
+    # client.command("insert into Animal set name = 'rat', specie = 'rodent'")
--- flask_orientdb.py
+++ flask_orientdb.py
@@ -7,19 +7,28 @@ import pyorient
 
 
 class OrientDB(object):
-    def __init__(self, app=None, password=None, username='root', host='localhost', port=2424):
-
+    def __init__(self, app=None, database_name=None, database_username='admin', database_password='admin',
+                 password=None, username='root',host='localhost', port=2424):
         if app is not None:
             self.app = app
-            self.init_app(self.app)
+            self.init_app(self.app, database_name=database_name, database_username=database_username,
+                          database_password=database_password, password=password, username=username, host=host, port=port)
         else:
             self.app = None
 
-    def init_app(self, app):
+    def init_app(self, app, database_name=None, database_username="admin", database_password="admin", password=None,
+                 username='root', host='localhost', port=2424):
         """
         """
         self.app = app
-        self.config = config 
+        self.app.config.setdefault('ORIENTDB_CURRENT_DATABASE', database_name)
+        self.app.config.setdefault('ORIENTDB_DATABASE_USERNAME', database_username)
+        self.app.config.setdefault('ORIENTDB_DATABASE_PASSWORD', database_password)
+        self.app.config.setdefault('ORIENTDB_AUTO_OPEN', True)
+        self.app.config.setdefault('ORIENTDB_SERVER_PASSWORD', password)
+        self.app.config.setdefault('ORIENTDB_SERVER_USERNAME', username)
+        self.app.config.setdefault('ORIENTDB_HOST', host)
+        self.app.config.setdefault('ORIENTDB_PORT', port)
         # Use the newstyle teardown_appcontext if it's available,
         # otherwise fall back to the request context
         if hasattr(app, 'teardown_appcontext'):
@@ -29,12 +38,6 @@ class OrientDB(object):
         # register extension with app only to say "I'm here"
         app.extensions = getattr(app, 'extensions', {})
         app.extensions['orientdb'] = self
-
-    def config(self, password, username, host, port):
-        self.app.config.setdefault('ORIENTDB_PASSWORD', password)
-        self.app.config.setdefault('ORIENTDB_USERNAME', username)
-        self.app.config.setdefault('ORIENTDB_HOST', host)
-        self.app.config.setdefault('ORIENTDB_PORT', port)
 
     @property
     def connected(self):
@@ -55,17 +58,20 @@ class OrientDB(object):
         if not self.connected:
             # TODO figure out how to use ctx.top
             if not hasattr(ctx, 'orient_db_client'):
-                ctx.orient_db_client = OrientDBPy(self.app.config.get('ORIENTDB_HOST'), self.app.config.get('ORIENTDB_PORT'))
+                ctx.orient_db_client = OrientDBPy(self.app.config.get('ORIENTDB_HOST'),
+                                                  self.app.config.get('ORIENTDB_PORT'))
 
             if not hasattr(ctx, 'orient_db_connection'):
-                ctx.orient_db_connection = ctx.orient_db_client.connect(self.app.config.get('ORIENTDB_USERNAME'),
-                                                                      self.app.config.get('ORIENTDB_PASSWORD'))
-
-        # TODO load record?
+                ctx.orient_db_connection = ctx.orient_db_client.connect(self.app.config.get('ORIENTDB_SERVER_USERNAME'),
+                                                                self.app.config.get('ORIENTDB_SERVER_PASSWORD'))
         def wrapper(*args, **kw):
+            if name != 'db_open' and self.app.config.get('ORIENTDB_AUTO_OPEN') and\
+                    self.app.config.get('ORIENTDB_CURRENT_DATABASE'):
+                        ctx.orient_db_client.db_open(self.app.config.get('ORIENTDB_CURRENT_DATABASE'),
+                        self.app.config.get('ORIENTDB_DATABASE_USERNAME'),  self.app.config.get('ORIENTDB_DATABASE_PASSWORD'))
             if name == 'db_create' and not args:
                 args = (pyorient.DB_TYPE_GRAPH, pyorient.STORAGE_TYPE_MEMORY)
-            # check if this is needed
+            # TODO check if this is needed
             if name == 'db_exists' and len(args) == 1:
                 db_name = args[0]
                 args = (db_name, pyorient.STORAGE_TYPE_MEMORY)
@@ -74,9 +80,15 @@ class OrientDB(object):
                 args = (cluster_name, pyorient.CLUSTER_TYPE_PHYSICAL)
             if name == 'db_open':
                 arg1 = args[0]
-                args = (arg1, 'admin', 'admin')
+                # TODO check order username, password
+                args = (arg1, 'ORIENTDB_DATABASE_USERNAME', 'ORIENTDB_DATABASE_PASSWORD')
+            # TODO load record? check if needed for other methods
             return getattr(ctx.orient_db_client, name)(*args, **kw)
         return wrapper
 
     def __setitem__(self, key, value):
-        pass
+        # TODO unhardcode
+        orient_config_list = ['ORIENTDB_SERVER_PASSWORD', 'ORIENTDB_SERVER_USERNAME', 'ORIENTDB_HOST', 'ORIENTDB_PORT',
+                              'ORIENTDB_CURRENT_DATABASE', 'ORIENTDB_DATABASE_USERNAME', 'ORIENTDB_DATABASE_PASSWORD']
+        if key in orient_config_list:
+            self.app.config[key] = value
